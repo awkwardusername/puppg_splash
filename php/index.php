54894504fadb025823d5ce260ab464fe018156b8
awkwardusername <markjayson.fuentes@pup.edu.ph>
2013-05-15 00:12:53 +0800

    google analytics

--- php/index.php
+++ php/index.php
@@ -5,14 +5,16 @@
     <title>Polytechnic University of the Philippines &mdash; Programming Guild</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="icon" type="image/x-icon" href="favicon.png" />
-    <script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-40907859-1']);
+	  _gaq.push(['_trackPageview']);
 
-	  ga('create', 'UA-40907859-1', 'demo-progguild.rhcloud.com');
-	  ga('send', 'pageview');
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
 	</script>
 </head>
 <body> <div id="body_main" class="body"> <p class="catchphrase"><em class="strong">Bored?</em></p> <p>Wanna do something <em class="stronger">great?</em></p> <p class="smile">:D</p> <p> <script>dateFuture1=new Date(2013,5,15,0,0,0);function GetCount(b,a){dateNow=new Date();amount=b.getTime()-dateNow.getTime();delete dateNow;if(amount<0){document.getElementById(a).innerHTML="Now!"}else{days=0;hours=0;mins=0;secs=0;out="";ms=(""+((amount%1000)+1000)).substr(1,3);amount=Math.floor(amount/1000);days=Math.floor(amount/86400);amount=amount%86400;hours=Math.floor(amount/3600);amount=amount%3600;mins=Math.floor(amount/60);amount=amount%60;secs=Math.floor(amount);out+=days+" : ";out+=hours+" : ";out+=mins+" : ";out+=secs+("."+ms)+" : ";out=out.substr(0,out.length-2);document.getElementById(a).innerHTML=out;setTimeout(function(){GetCount(b,a)},25)}}window.onload=function(){GetCount(dateFuture1,"timer")};</script><div id="timer"></div> </p> </div> <div id="footer_main" class="footer"> <ul class="networks"> <li><a href="http://www.facebook.com/PUPProgrammingGuild" target="_blank"><img src="img/icon-facebook.svg" alt="Website"> PUPProgrammingGuild</a></li> <li><a href="http://twitter.com/pup_progguild" target="_blank"><img src="img/icon-twitter.svg" alt="Website"> @pup_progguild</a></li> </ul> </div></body></html>
